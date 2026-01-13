8998bd1e37d5d0d907cd6f7990085af0b87e5aad
Flow <devnull@example.com>
2026-01-12 22:56:19 -0600

    style: format benchmark code for consistency

--- pkg/flow/context_pool_bench_test.go
+++ pkg/flow/context_pool_bench_test.go
@@ -8,20 +8,20 @@ var benchSink *Context
 // Microbenchmarks to compare allocating Context directly vs using the
 // pool-backed NewContext + PutContext.
 func BenchmarkContextPool(b *testing.B) {
-    b.Run("alloc_direct", func(b *testing.B) {
-        b.ReportAllocs()
-        for i := 0; i < b.N; i++ {
-            c := &Context{}
-            benchSink = c
-        }
-    })
+	b.Run("alloc_direct", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := &Context{}
+			benchSink = c
+		}
+	})
 
-    b.Run("pooled_new_put", func(b *testing.B) {
-        b.ReportAllocs()
-        for i := 0; i < b.N; i++ {
-            c := NewContext(nil, nil, nil)
-            benchSink = c
-            PutContext(c)
-        }
-    })
+	b.Run("pooled_new_put", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := NewContext(nil, nil, nil)
+			benchSink = c
+			PutContext(c)
+		}
+	})
 }
