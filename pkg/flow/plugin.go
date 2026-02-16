5e57010aed34894a5613e127caacd1e3b53fd4ff
Flow <devnull@example.com>
2026-02-16 13:46:35 -0600

    refactor: move ServiceRegistry implementation into service_registry.go (cleanup)

--- pkg/flow/plugin.go
+++ pkg/flow/plugin.go
@@ -39,35 +39,6 @@ var (
 	ErrPluginIncompatibleMajor = errors.New("plugin: incompatible major version")
 )
 
-// Register registers a service under the provided name. If a service is
-// already registered with the same name an error is returned.
-func (r *ServiceRegistry) Register(name string, svc interface{}) error {
-	if name == "" {
-		return fmt.Errorf("service name cannot be empty")
-	}
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if _, ok := r.services[name]; ok {
-		return fmt.Errorf("service already registered: %s", name)
-	}
-	r.services[name] = svc
-	return nil
-}
-
-// Get returns the service and a boolean indicating whether it was found.
-func (r *ServiceRegistry) Get(name string) (interface{}, bool) {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
-	s, ok := r.services[name]
-	return s, ok
-}
-
-// GetAs returns the service registered under name cast to the requested type T.
-// If the service is not found or cannot be asserted to T the zero value and
-// false are returned. Implemented as a package-level generic helper because
-// Go does not allow methods with independent type parameters on non-generic
-// receiver types.
-
 // PluginAPIMajor is the major version of the plugin API expected by this
 // version of the framework. Plugins with a differing major semantic version
 // are considered incompatible. Bump this constant when making breaking
